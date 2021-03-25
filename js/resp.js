72b1426572a5cb92cf68d1b99b04c5db2e00d6da
princekumar6 <37827035+princekumar6@users.noreply.github.com>
2021-03-25 19:51:45 +0530

    Add files via upload

--- js/resp.js
+++ js/resp.js
@@ -1,14 +1,23 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-a = document.querySelectorAll('.navbar .options')
+// burger = document.querySelector('.burger')
+// navbar = document.querySelector('.navbar')
+// a = document.querySelectorAll('.navbar .options')
 
-burger.addEventListener('click',()=>{
-    for (i = 0; i < a.length; i++) {
-        a[i].classList.toggle('v-class-resp');
-    }
-    navbar.classList.toggle('h-nav-resp');
-})
+// burger.addEventListener('click',()=>{
+//     for (i = 0; i < a.length; i++) {
+//         a[i].classList.toggle('v-class-resp');
+//     }
+//     navbar.classList.toggle('h-nav-resp');
+// })
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    //document.getElementById("header").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("header").style.marginLeft= "0";
+};
 function myFunction() {
     var x = document.getElementById("password-field");
     if (x.type === "password") {
@@ -17,3 +26,15 @@ function myFunction() {
         x.type = "password";
     }
 }
+
+function myFunction_two() {
+    var x = document.getElementById("myDIV");
+        x.style.display = "block";
+    
+}
+function myFunction_three() {
+    var x = document.getElementById("menu");
+        x.style.display = "block";
+    
+}
+
